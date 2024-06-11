2e1e6f133be618cb9fa5ab6464706f42b7349d81
FrederikkeGylstorff1 <frederikke@sneugle.dk>
2024-06-11 16:25:05 +0200

    migration imageurl

---
+++ RestApiLK/Migrations/20240610220858_AddImageUrlToProdukter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestApiLK.Migrations
+{
+    public partial class AddImageUrlToProdukter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Produkter",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Produkter");
+        }
+    }
+}
